0f2d541dcd905f4c9403d6bdac02d1feb6eece64
dcasseb <77282586+dcasseb@users.noreply.github.com>
2023-05-02 21:37:27 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,12 @@ import model.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		ArrayList<Filial> filiais = new ArrayList<Filial>();
-		
-		Filial filial = new Filial("Lago Sul", 1, null);
-		
+		ArrayList<Filial> filiais = new ArrayList<Filial>();		
+		Filial filial = new Filial("Lago Sul", 16785, null);	
 		System.out.println(filial);
+		
+		ArrayList<Patrimonio> patrimonios = new ArrayList<Patrimonio>();
+		Patrimonio patrimonio = new Patrimonio("Cadeira", 40);
+		System.out.println(patrimonio);
 	}
 }
