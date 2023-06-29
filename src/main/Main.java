f932b26c07863713dbe7bc263256d7da9468d07f
dcasseb <77282586+dcasseb@users.noreply.github.com>
2023-06-29 11:28:38 -0300

    Revert "Update Main.java"
    This reverts commit 0f2d541dcd905f4c9403d6bdac02d1feb6eece64.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,12 +6,10 @@ import model.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		ArrayList<Filial> filiais = new ArrayList<Filial>();		
-		Filial filial = new Filial("Lago Sul", 16785, null);	
-		System.out.println(filial);
+		ArrayList<Filial> filiais = new ArrayList<Filial>();
 		
-		ArrayList<Patrimonio> patrimonios = new ArrayList<Patrimonio>();
-		Patrimonio patrimonio = new Patrimonio("Cadeira", 40);
-		System.out.println(patrimonio);
+		Filial filial = new Filial("Lago Sul", 1, null);
+		
+		System.out.println(filial);
 	}
 }
